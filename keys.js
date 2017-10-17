ccfe617d68cf59c05f28dc24b69efbb7b9786a77
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2017-10-17 16:05:05 -0400

    Initialize

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+	consumer_key: 'oVnsS9bMqOWsvFIphSSUQk2KS',
+	consumer_secret: 'u6Bqy51rmcXoC9G4Dg0oeCUttFOanoI6nYDVcJiLJU5Rksup0B',
+	access_token_key: '920374447428833281-4utWgkN4R9pSlkwlo0biDQFRfZw4VFg',
+	access_token_secret: 'uiuve4NzM5fzQO3AjhIh4YBurSSHYgR5Xmui8vyg29MzA'
+}
+module.exports = twitterKeys;
