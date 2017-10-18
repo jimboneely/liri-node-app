c4109370d53e414dc5b8ab7a60c2cee48b6d26db
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2017-10-18 19:41:58 -0400

    Package Updates

--- liri.js
+++ liri.js
@@ -1,6 +1,4 @@
 var fs = require("fs");
 var request = require("request");
-var twitterKeys = require("./twitterKeys");
-var spotifyKeys = require("./spotifyKeys");
-
+var twitterKeys = require("./keys.js");
 
