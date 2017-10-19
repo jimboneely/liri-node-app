76d51fb06be027ad009334eb24c10e6318a4c332
James Neely <jimboneely@Jamess-MacBook-Pro.local>
2017-10-18 23:05:35 -0400

    Spotify and Twitter Function

--- keys.js
+++ keys.js
@@ -1,29 +1,20 @@
 console.log('this is loaded');
 
-var Twitter = require('twitter');
-
-var twitterKeys = new Twitter({
+var twitterKeys = {
 	consumer_key: 'oVnsS9bMqOWsvFIphSSUQk2KS',
 	consumer_secret: 'u6Bqy51rmcXoC9G4Dg0oeCUttFOanoI6nYDVcJiLJU5Rksup0B',
 	access_token_key: '920374447428833281-4utWgkN4R9pSlkwlo0biDQFRfZw4VFg',
 	access_token_secret: 'uiuve4NzM5fzQO3AjhIh4YBurSSHYgR5Xmui8vyg29MzA'
-});
+};
 
-var params = {screen_name: 'wakandaCode'};
-twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  	for (var i = 0; i < tweets.length; i++) {
-  		console.log(tweets[i].text);
-  	}
-  }
-
-});
-
-module.exports = twitterKeys;
 
 var spotifyKeys = {
-	client_ID: '0e8dcb3d679644c08cfe932e8e96624a',
-	client_secret: '8d43b82472574dfcb710e796913aa96f',
+	id: '0e8dcb3d679644c08cfe932e8e96624a',
+	secret: '8d43b82472574dfcb710e796913aa96f',
 
 }
-module.exports = spotifyKeys;
+module.exports = {spotify:spotifyKeys, twitter:twitterKeys};
+
+
+// client_ID
+// client_secret
--- liri.js
+++ liri.js
@@ -1,4 +1,57 @@
 var fs = require("fs");
 var request = require("request");
-var twitterKeys = require("./keys.js");
+var keys = require("./keys");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var apiCall = process.argv[2];
+
+
+switch (apiCall){
+	case text = "my-tweets":
+			var twitterClient = new Twitter(keys.twitter);
+			var params = {screen_name: 'wakandaCode'};
+			twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+ 			if (error) {
+ 			console.log(error);
+ 				}
+ 			else if (!error) {
+  			for (var i = 0; i < tweets.length; i++) {
+  			console.log(tweets[i].text);
+  				}
+			};
+			});
+			break;
+	case text = "spotify-this-song":
+			var spotifyClient = new Spotify(keys.spotify);
+			spotifyClient.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  			if (err) {
+    		return console.log('Error occurred: ' + err);
+  			}
+ 
+			console.log(data); 
+});
+			break;
+	default: 
+			console.log('Huh?');
+
+};
+
+
+
+
+// 	var twitterClient = new Twitter(keys.twitter);
+// 	var params = {screen_name: 'wakandaCode'};
+// 	twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
+//  	if (error) {
+//  		console.log(error);
+//  	}
+//  	else if (!error) {
+//   	for (var i = 0; i < tweets.length; i++) {
+//   	console.log(tweets[i].text);
+//   	}
+// };
+
+// });
+
+// };
 
